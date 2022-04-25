2a6759c4012e2da7d79a4ee4cdd519d6b7e18840
indraworks <indra.works2020@gmail.com>
2022-04-25 10:03:45 +0700

    2-useEffect-fetch-Data

--- src/tutorial/2-useEffect/setup/3-useEffect-fetch-data.js
+++ src/tutorial/2-useEffect/setup/3-useEffect-fetch-data.js
@@ -1,9 +1,52 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react"
 
-const url = 'https://api.github.com/users';
+const url = "https://api.github.com/users"
 
 const UseEffectFetchData = () => {
-  return <h2>fetch data</h2>;
-};
+  const [users, setUsers] = useState([])
 
-export default UseEffectFetchData;
+  const getUsers = async () => {
+    const response = await fetch(url)
+    const data = await response.json()
+    setUsers(data)
+    console.log(users)
+  }
+
+  useEffect(() => {
+    getUsers()
+    // eslint-disable-next-line
+  }, [])
+  return (
+    <>
+      <h2>fetch data github users</h2>
+      <ul>
+        {users.map((user) => {
+          const { id, login, avatar_url, html_url } = user
+          return (
+            <div className='users'>
+              <li key={id}>
+                <img src={avatar_url} alt={login} />
+                <div>
+                  <h4>{login}</h4>
+                  <a href={html_url}>Profile</a>
+                </div>
+              </li>
+            </div>
+          )
+        })}
+      </ul>
+    </>
+  )
+}
+
+export default UseEffectFetchData
+
+/*
+disni kit aakan gunakan useEffect utk fecth data bisa dgn axios 
+atau nisa dgn fetch kalau dgn fetch harus 2x :
+-ambil sbgai response , 2 ambil sebgai data yaitu response.data atau response.json
+disni data yg kita ambil adalah data dari github users
+
+css ny agunaka .users dari global.css
+
+*/
