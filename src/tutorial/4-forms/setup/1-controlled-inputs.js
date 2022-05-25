365af2a2626f5c023eef6f815ecc1756095ba472
indraworks <indra.works2020@gmail.com>
2022-05-26 06:28:02 +0700

    58.form basics

--- src/tutorial/4-forms/setup/1-controlled-inputs.js
+++ src/tutorial/4-forms/setup/1-controlled-inputs.js
@@ -19,8 +19,8 @@ const handleSubmit = (e) => {
 const ControlledInputs = () => {
   return (
     <>
-      <h1>controlled inputs</h1>
-      <form className='form'>
+      <h4>controlled inputs</h4>
+      <form className='form' onSubmit={handleSubmit}>
         <div className='form-control'>
           <label htmlFor='firstname'>Name</label>
           <input
@@ -34,12 +34,31 @@ const ControlledInputs = () => {
           <label htmlFor='email'>Email</label>
           <input type='text' placeholder='email' id='email' name='email' />
         </div>
-        <button type='submit' onClick={handleSubmit}>
-          Submit
-        </button>
+        <button type='submit'>Submit</button>
       </form>
     </>
   )
 }
 
 export default ControlledInputs
+
+/*
+
+sebagai caatan bisanya utk form ada action namanya handke submit nah 
+itu dari form dgn panggil functionnya 
+nah bisa juga dgn tombol dari button dimana button masih anggota control dari 
+form ,tapy buttonya harus bertype ="submit" 
+cara 
+1)bisa dgn onClikc , onsubmit di form di ksoongkan 
+bisa di ketahui dari button dgn type submit:
+<button type='submit' onClick={handleSubmit}>
+          Submit
+        </button>
+
+2)atau dari form dimana  <form onSubmit={handleSubmit}>
+dan buttonya tak perlu pakai onCLikc 
+
+
+
+
+*/
