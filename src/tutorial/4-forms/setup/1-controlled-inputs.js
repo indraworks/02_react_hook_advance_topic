1cfc78379b525fc88718f5e67f819678e33ff635
indraworks <indra.works2020@gmail.com>
2022-05-25 09:59:38 +0700

    58.form basic

--- src/tutorial/4-forms/setup/1-controlled-inputs.js
+++ src/tutorial/4-forms/setup/1-controlled-inputs.js
@@ -1,12 +1,45 @@
-import React, { useState } from 'react';
+import React, { useState } from "react"
 // JS
 // const input = document.getElementById('myText');
 // const inputValue = input.value
 // React
 // value, onChange
+/*
+sebagai caatan bisanya utk form ada action namanya handke submit nah 
+itu dari form dgn panggil functionnya 
+nah bisa juga dgn tombol dari button dimana button masih anggota control dari 
+form ,tapy buttonya harus bertype ="submit" 
+
+*/
+const handleSubmit = (e) => {
+  e.preventDefault() //utk hindary browser refresh
+  console.log("hello world")
+}
 
 const ControlledInputs = () => {
-  return <h1>controlled inputs</h1>;
-};
+  return (
+    <>
+      <h1>controlled inputs</h1>
+      <form className='form'>
+        <div className='form-control'>
+          <label htmlFor='firstname'>Name</label>
+          <input
+            type='text'
+            id='firstName'
+            placeholder='firstName'
+            name='firstName'
+          />
+        </div>
+        <div className='form-control'>
+          <label htmlFor='email'>Email</label>
+          <input type='text' placeholder='email' id='email' name='email' />
+        </div>
+        <button type='submit' onClick={handleSubmit}>
+          Submit
+        </button>
+      </form>
+    </>
+  )
+}
 
-export default ControlledInputs;
+export default ControlledInputs
